e36104f30037492ee65d55c837eb546a19737135
George Okwori <132067023+GOkwori@users.noreply.github.com>
2024-06-14 10:29:29 +0100

    Register order and order_line_item models in admin site

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import order, order_line_item
 
-# Register your models here.
+
+class orderLineAdminInline(admin.TabularInline):
+    model = order_line_item
+    readonly_fields = ('lineitem_total',)
+
+
+class orderAdmin(admin.ModelAdmin):
+    inlines = (orderLineAdminInline,)
+
+    readonly_fields = ('order_reference', 'date',
+                       'order_total', 'delivery_cost', 'grand_total')
+
+    fields = ('order_reference', 'full_name', 'phone_number',
+              'country', 'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'date', 'order_total',
+              'delivery_cost', 'grand_total')
+
+    list_display = ('order_reference', 'date', 'full_name',
+                    'order_total', 'delivery_cost', 'grand_total')
+
+    ordering = ('-date',)
+
+
+admin.site.register(order, orderAdmin)
