255210d5cd9ea820672b1282eaec4ce64cc11ee5
George Okwori <132067023+GOkwori@users.noreply.github.com>
2024-06-16 18:02:47 +0100

    Update settings.py to include AWS S3 integration for static and media file storage

--- grizk/settings.py
+++ grizk/settings.py
@@ -83,8 +83,8 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                'django.template.context_processors.media',  # new
-                'cart.contexts.cart_contents',  # new
+                'django.template.context_processors.media',
+                'cart.contexts.cart_contents',
             ],
             'builtins': ['crispy_forms.templatetags.crispy_forms_tags',
                          'crispy_forms.templatetags.crispy_forms_field'
