3fe69e57b7a65ca279d853a9e8ed598db9cab012
George Okwori <132067023+GOkwori@users.noreply.github.com>
2024-07-08 00:20:01 +0100

    Update default country label in UserProfile model

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
@@ -8,9 +9,14 @@ def profile(request):
     Display the user's profile
     """
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    form = UserProfileForm(instance=profile)
+    order_history = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'order_history': order_history,
     }
 
     return render(request, template, context)
