42c0cd35fe7a637635cb07d5291c1495a7f9a0b0
George Okwori <132067023+GOkwori@users.noreply.github.com>
2024-07-12 17:20:14 +0100

    Update SECRET_KEY handling in settings.py

--- grizk/settings.py
+++ grizk/settings.py
@@ -10,10 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-hdt0q9gm0^6mj!4mi)=zyu(+72w5yop!wek6)+^x)+wmp0=w8%"
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ecommerce-grizk-2f04b3042fc5.herokuapp.com', 'localhost', ]
 
