38513f7c903a57aeffaed3298c026b061d94f769
George Okwori <132067023+GOkwori@users.noreply.github.com>
2024-07-26 00:04:31 +0100

    Update cart templates and styles for displaying product information and images

--- cart/views.py
+++ cart/views.py
@@ -14,31 +14,25 @@ def view_cart(request):
 
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
-
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    colour = None
-    if 'product_colour' in request.POST:
-        colour = request.POST['product_colour']
+    colour = request.POST.get('product_colour', None)
     cart = request.session.get('cart', {})
 
     if colour:
-        if item_id in list(cart.keys()):
-            if colour in cart[item_id]['items_by_colour'].keys():
-                cart[item_id]['items_by_colour'][colour] += quantity
-                messages.success(
-                    request, f'Updated colour {colour.upper()} {product.name} quantity to {cart[item_id]["items_by_colour"][colour]}')
-            else:
-                cart[item_id]['items_by_colour'][colour] = quantity
-                messages.success(
-                    request, f'Added colour {colour.upper()} {product.name} to your cart')
+        if item_id not in cart:
+            cart[item_id] = {'items_by_colour': {}}
+        if colour in cart[item_id]['items_by_colour']:
+            cart[item_id]['items_by_colour'][colour] += quantity
+            messages.success(
+                request, f'Updated colour {colour.upper()} {product.name} quantity to {cart[item_id]["items_by_colour"][colour]}')
         else:
-            cart[item_id] = {'items_by_colour': {colour: quantity}}
+            cart[item_id]['items_by_colour'][colour] = quantity
             messages.success(
                 request, f'Added colour {colour.upper()} {product.name} to your cart')
     else:
-        if item_id in list(cart.keys()):
+        if item_id in cart:
             cart[item_id] += quantity
             messages.success(
                 request, f'Updated {product.name} quantity to {cart[item_id]}')
@@ -52,25 +46,23 @@ def add_to_cart(request, item_id):
 
 def adjust_cart(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
-
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
-    colour = None
-    if 'product_colour' in request.POST:
-        colour = request.POST['product_colour']
+    colour = request.POST.get('product_colour', None)
     cart = request.session.get('cart', {})
 
     if colour:
-        if quantity > 0:
-            cart[item_id]['items_by_colour'][colour] = quantity
-            messages.success(
-                request, f'Updated colour {colour.upper()} {product.name} quantity to {cart[item_id]["items_by_colour"][colour]}')
-        else:
-            del cart[item_id]['items_by_colour'][colour]
-            if not cart[item_id]['items_by_colour']:
-                cart.pop(item_id)
-            messages.success(
-                request, f'Removed colour {colour.upper()} {product.name} from your cart')
+        if item_id in cart and colour in cart[item_id]['items_by_colour']:
+            if quantity > 0:
+                cart[item_id]['items_by_colour'][colour] = quantity
+                messages.success(
+                    request, f'Updated colour {colour.upper()} {product.name} quantity to {cart[item_id]["items_by_colour"][colour]}')
+            else:
+                del cart[item_id]['items_by_colour'][colour]
+                if not cart[item_id]['items_by_colour']:
+                    cart.pop(item_id)
+                messages.success(
+                    request, f'Removed colour {colour.upper()} {product.name} from your cart')
     else:
         if quantity > 0:
             cart[item_id] = quantity
@@ -86,23 +78,23 @@ def adjust_cart(request, item_id):
 
 def remove_from_cart(request, item_id):
     """Remove the item from the shopping cart"""
-
     try:
         product = get_object_or_404(Product, pk=item_id)
-        colour = None
-        if 'product_colour' in request.POST:
-            colour = request.POST['product_colour']
+        colour = request.POST.get('product_colour', None)
         cart = request.session.get('cart', {})
 
         if colour:
-            del cart[item_id]['items_by_colour'][colour]
-            if not cart[item_id]['items_by_colour']:
-                cart.pop(item_id)
-            messages.success(
-                request, f'Removed colour {colour.upper()} {product.name} from your cart')
+            if item_id in cart and colour in cart[item_id]['items_by_colour']:
+                del cart[item_id]['items_by_colour'][colour]
+                if not cart[item_id]['items_by_colour']:
+                    cart.pop(item_id)
+                messages.success(
+                    request, f'Removed colour {colour.upper()} {product.name} from your cart')
         else:
-            cart.pop(item_id)
-            messages.success(request, f'Removed {product.name} from your cart')
+            if item_id in cart:
+                cart.pop(item_id)
+                messages.success(
+                    request, f'Removed {product.name} from your cart')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
