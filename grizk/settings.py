1b9fec7ec7b4e7c22dab7c53a3c7df9b89816c94
George Okwori <132067023+GOkwori@users.noreply.github.com>
2024-09-11 20:41:21 +0100

    chore: Update email backend configuration for development and production environments

--- grizk/settings.py
+++ grizk/settings.py
@@ -194,11 +194,11 @@ if env('DEVELOPMENT', default=False):
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASS', default='')
-DEFAULT_FROM_EMAIL = env('EMAIL_HOST_USER', default='')
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='')
+    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASS', default='')
+    DEFAULT_FROM_EMAIL = env('EMAIL_HOST_USER', default='')
 
 
 # Default primary key field type
