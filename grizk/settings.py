4a1536346d67d71072a86d4dd69fff7feacf7ffd
George Okwori <132067023+GOkwori@users.noreply.github.com>
2024-09-11 21:22:36 +0100

    chore: Update SECRET_KEY and DEBUG settings in settings.py

--- grizk/settings.py
+++ grizk/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY', default='')
+SECRET_KEY = env('SECRET_KEY', default='your-default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=True)
