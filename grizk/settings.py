2558ce4adc64b5d69d333b59a59088b4bb5b7866
George Okwori <george.okwori@outlook.com>
2024-09-18 23:42:53 +0000

    Create json for loading new products

--- grizk/settings.py
+++ grizk/settings.py
@@ -119,6 +119,7 @@ WSGI_APPLICATION = "grizk.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
@@ -127,10 +128,10 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
+    
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
