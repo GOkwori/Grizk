7417d28d037eab311cb6cd92b6cf1cd1f3a7b6c4
George Okwori <george.okwori@outlook.com>
2024-10-04 04:49:32 +0000

    Update checkout model

--- checkout/models.py
+++ checkout/models.py
@@ -37,7 +37,7 @@ class Order(models.Model):
     stripe_pid = models.CharField(
         max_length=254, null=False, blank=False, default='')
 
-    def _generate_order_number(self):
+    def _generate_order_reference(self):
         """ Generate a random, unique order number using UUID """
         return uuid.uuid4().hex.upper()
 
@@ -59,14 +59,14 @@ class Order(models.Model):
     def save(self, *args, **kwargs):
         """
         Override the original save method to set the order
-        number if it hasnt been set already
+        number if it hasn't been set already
         """
-        if not self.order_number:
-            self.order_number = self._generate_order_number()
+        if not self.order_reference:
+            self.order_reference = self._generate_order_reference()
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return self.order_number
+        return self.order_reference
 
 
 class OrderLineItem(models.Model):
@@ -93,4 +93,4 @@ class OrderLineItem(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'SKU {self.product.sku} on order {self.order.order_number}'
+        return f'SKU {self.product.sku} on order {self.order.order_reference}'
